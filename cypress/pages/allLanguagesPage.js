dca97c215e70bbad08ae070a92866c7e97294234
yuliiaanhurets <yangurets@gmail.com>
2024-03-14 14:11:50 +0200

    Removed test

--- cypress/pages/allLanguagesPage.js
+++
@@ -1,9 +0,0 @@
-class allLanguagesPage {
-
-    elements = {
-        languagesList: () => cy.get("[dir='ltr'] a"),
-        languageEN: () => cy.get('#js-link-box-en'),
-    }
-}
-
-module.exports = new allLanguagesPage();
